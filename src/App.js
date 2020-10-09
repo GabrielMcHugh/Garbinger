33d198bfeefae7c05a95260ebc2a8c8f325de66e
Gabriel McHugh <gabriel.t.mchugh@gmail.com>
2020-10-09 12:33:04 +1000

    started entrypage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // constructor() {
+  // }
+
+  componentDidMount(){
+  }
+
+  render () {
+    return(
+      <main>
+        <h1>Hi!</h1>
+        <p>We understand recyling can be hard. Select what you are disposing and we will help you out!</p>
+        <button >
+          SORT ME OUT!
+        </button>
+      </main>
+    )
+  };
 }
 
 export default App;
